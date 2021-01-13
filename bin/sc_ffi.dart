d75c3b7054e9063a92c4969237280f98bed5276b
Aditya Kishore <akushwarrior@gmail.com>
2021-01-12 22:13:25 -0800

    initial commit

---
+++ bin/sc_ffi.dart
@@ -0,0 +1,5 @@
+import 'package:sc_ffi/sc_ffi.dart' as sc_ffi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sc_ffi.calculate()}!');
+}
---
+++ test/sc_ffi_test.dart
@@ -0,0 +1,8 @@
+import 'package:sc_ffi/sc_ffi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
